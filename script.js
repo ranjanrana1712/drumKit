481b068f94b18c20f06ec1f082c05ffcb2b66114
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2023-12-17 14:46:51 +0530

    update: need to make responsive

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-function removeTransition(e) {
-  if (e.propertyName != 'transform') return;//check if the propertyName of the event 'e' is not equal to 'tranform'
-  e.target.classList.remove('playing'); //it removes 'playing' class from element that triggered transition.
-}
+
 
 function playSound (e) {
 
+  console.log("playSound called");
+
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
 
-  const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
 
   if(!audio) return;
 
   key.classList.add('playing');//This allow to use classes to animate the visual representation.
-  audio.currentTime = 1;//This ensures that the audio starts from beginning each time the key is pressed
+  audio.currentTime = 0;//This ensures that the audio starts from beginning each time the key is pressed
   audio.play();//This line initiates playback of the selected audio element
 
 }
+function removeTransition(e) {
+  if (e.propertyName !=='transform') return;//check if the propertyName of the event 'e' is not equal to 'tranform'
+  this.classList.remove('playing'); //it removes 'playing' class from element that triggered transition.
+  console.log('transition called')
+  
+}
 
-const keys = Array.from(document.querySelectorAll('.key'));//converting NodeList into an array using Array.from()
-keys.forEach(keys => keys.addEventListener('transitioned', removeTransition)); // adds transitionend event listener to each element.
-  // window.addEventListener('keydown', playSound);
+const keys = document.querySelectorAll('.key');//converting NodeList into an array using Array.from()
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); // adds transitionend event listener to each element.
 window.addEventListener('keydown', playSound);
